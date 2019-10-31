70c4d7d39f8baca9648058aebf3516dc8e7286dc
AlNualD <nuald@outlook.com>
2019-10-31 08:31:04 +0300

    maybe that is the problem

--- include/task3.h
+++ include/task3.h
@@ -3,11 +3,11 @@
 
 using namespace std;
 
-template <typename T>
-T change(T elem)
-{
-	return elem + 1;
-}
+// template <typename T>
+// T change(T elem)
+// {
+// 	return elem + 1;
+// }
 
 template <typename T, size_t N>
 void map(T (&arr)[N], T(*change)(T))
