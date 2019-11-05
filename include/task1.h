c6493ef890d32efdabe9fa9911b60d7267d30550
mp913 <zarkov01@mail.ru>
2019-11-05 11:44:51 +0300

    Compare function fixed.

--- include/task1.h
+++ include/task1.h
@@ -14,14 +14,14 @@ bool compare(T first, T second)
 template <>
 bool compare(char* first, char* second)
 {
-	if (strlen(first) == strlen(second)
+	if (strlen(first) == strlen(second))
 	{
 		int ind = 0;
-		while (ind < strlen(first) && first[i] == second[ind])
+		while (ind < strlen(first) && first[i] == second[ind]))
 		{
 			ind++;
 		}
-		if (ind == strlen(first) return false;
+		if (ind == strlen(first)) return false;
 		return first[ind] > second[ind];
 	}
 	return strlen(first) > strlen(second);
