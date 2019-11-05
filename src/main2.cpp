350d280b38e283e3942c2d41f0173aead15e1317
mp913 <zarkov01@mail.ru>
2019-11-05 11:01:05 +0300

    Decommented "main1" and "main2".

--- src/main2.cpp
+++ src/main2.cpp
@@ -6,13 +6,13 @@ T gen() {
 	return 13;
 }
 
-//int main() {
-//	const signed n = 5;
-//	int* a = createArr<int, n>(gen);
-//	for (int i = 0; i < n; i++)
-//	{
-//		std::cout << a[i] << ' ';
-//	}
-//	std::cout << '\n';
-//	return 0;
-//}
+int main() {
+	const signed n = 5;
+	int* a = createArr<int, n>(gen);
+	for (int i = 0; i < n; i++)
+	{
+		std::cout << a[i] << ' ';
+	}
+	std::cout << '\n';
+	return 0;
+}
