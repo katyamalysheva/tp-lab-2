3cb7def06517437199f131bc1774eebb4ae2b11c
NoVarlok <yakhtin.leonid@yandex.com>
2019-11-05 19:06:38 +0300

    fix 6

--- include/task1.h
+++ include/task1.h
@@ -9,6 +9,15 @@
 using namespace std;
 
 template<class T>
+bool cmp(T a, T b) {
+	return a <= b;
+}
+
+bool cmp(char *a, char *b) {
+	return strlen(a) <= strlen(b);
+}
+
+template<class T>
 void msort(T *arr, int n) {
 	if (n == 1)return;
 	T *left = new T[n / 2];
@@ -28,55 +37,11 @@ void msort(T *arr, int n) {
 			pl++;
 		}
 		else {
-			if (left[pl] <= right[pr]) {
+			if (cmp(left[pl],right[pr])) {
 				arr[i] = left[pl];
 				pl++;
 			}
 			else {
-				arr[i] = right[pr];
-				pr++;
-			}
-		}
-	}
-}
-
-template<>
-void msort(char** arr, int n) {
-	if (n == 1)return;
-	char** left = new char*[n / 2];
-	for (int i = 0;i < n / 2;i++) {
-		left[i]= arr[i];
-	}
-	char** right = new char*[(n + 1) / 2];
-	for (int i = n / 2;i < n;i++) {
-		right[i - n / 2] = arr[i];
-	}
-	msort(left, n / 2);
-	msort(right, (n + 1) / 2);
-	int pl = 0, pr = 0;
-	for (int i = 0;i < n;i++) {
-		if (pl == n / 2) {
-			//strcpy(arr[i], right[pr]);
-			//swap(arr[i], right[pr]);
-			arr[i] = right[pr];
-			pr++;
-		}
-		else if (pr == (n + 1) / 2) {
-			//strcpy(arr[i], left[pl]);
-			//swap(arr[i], left[pl]);
-			arr[i] = left[pl];
-			pl++;
-		}
-		else {
-			if (strlen(left[pl]) < strlen(right[pr])) {
-				//strcpy(arr[i], left[pl]);
-				//swap(arr[i], left[pl]);
-				arr[i] = left[pl];
-				pl++;
-			}
-			else {
-				//strcpy(arr[i], right[pr]);
-				//swap(arr[i], right[pr]);
 				arr[i] = right[pr];
 				pr++;
 			}
